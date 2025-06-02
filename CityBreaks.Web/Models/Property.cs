5e6943bd150987220fb05d0cf509de51fac4f71f
jose ricardo kremer polesi <josericardo.kremer@gmail.com>
2025-06-01 23:13:18 -0300

    Add exercise 4

---
+++ CityBreaks.Web/Models/Property.cs
@@ -0,0 +1,11 @@
+namespace CityBreaks.Web.Models;
+
+public class Property
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal PricePerNight { get; set; }
+    
+    public int CityId { get; set; }     
+    public City City { get; set; }    
+}
