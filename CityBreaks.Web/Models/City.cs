22013ff9e631e724487cfcf32d2541504d53cddb
jose ricardo kremer polesi <josericardo.kremer@gmail.com>
2025-06-01 22:28:24 -0300

    Add exercise 3

---
+++ CityBreaks.Web/Models/City.cs
@@ -0,0 +1,9 @@
+namespace CityBreaks.Web.Models;
+
+public class City
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int CountryId { get; set; }
+    public Country Country { get; set; }
+}
--- CityBreaks.Web/Models/Country.cs
+++ CityBreaks.Web/Models/Country.cs
@@ -5,4 +5,5 @@ public class Country
     public int Id { get; set; }
     public string CountryCode { get; set; }
     public string CountryName { get; set; }
+    public List<City> Cities { get; set; }
 }
