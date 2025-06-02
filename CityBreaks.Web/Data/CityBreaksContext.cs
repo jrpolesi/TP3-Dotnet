0cb57e3d6472bf32f4857d2df663632cff4d997d
jose ricardo kremer polesi <josericardo.kremer@gmail.com>
2025-06-01 21:00:56 -0300

    Add exercise 1

---
+++ CityBreaks.Web/Data/CityBreaksContext.cs
@@ -0,0 +1,14 @@
+using CityBreaks.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityBreaks.Web.Data;
+
+public class CityBreaksContext : DbContext
+{
+    public CityBreaksContext(DbContextOptions<CityBreaksContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Country> Countries { get; set; }
+}
---
+++ CityBreaks.Web/Models/Country.cs
@@ -0,0 +1,7 @@
+namespace CityBreaks.Web.Models;
+
+public class Country
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
